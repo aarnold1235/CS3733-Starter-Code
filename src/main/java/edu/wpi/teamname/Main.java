9a0462af73821a48e81c9477ef0310718e97f300
Rohan Anand <rohananand@hotmail.com>
2021-03-25 15:25:39 -0400

    Added my name to main(Rohan)

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -15,7 +15,7 @@ public class Main {
     System.out.println("Payton Dean");
     System.out.println("Team Coach: Michael Clements");
     System.out.println("Danilo Correia");
-
     System.out.println("David Fournet");
+    System.out.println("Rohan Anand");
   }
 }
